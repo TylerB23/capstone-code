3494aad2fa3c9019d527164abb78f77d07aed05b
TylerB23 <tylerburleyson23@gmail.com>
2022-02-21 09:46:23 -0500

    finalized 1D model and 2D advection

--- symmetric_pde_model/simple_advection_1D.m
+++ symmetric_pde_model/simple_advection_1D.m
@@ -1,10 +1,10 @@
 %% Specify Parameters
 
 a = 1; % advection speed
-h = 0.01; % spatial step size
+h = 0.005; % spatial step size
 k = 0.005; % time step size
 x_bounds = [0 2];
-time_bounds = [0 10];
+time_bounds = [0 2];
 
 nx = (x_bounds(2) - x_bounds(1))/h; % number of x steps
 nt = (time_bounds(2) - time_bounds(1))/k;
@@ -15,10 +15,12 @@ rho = zeros(nx,nt); % density function
 
 %% Initial Conditions
 
-rho(x==1,1) = 1;
+% rho(x==1,1) = 1;
 
 % uniform distribution around 1
-%rho(1-5*h <= x & x <= 1+5*h,1) = round(rand(sum(1-5*h <=x & x<= 1+5*h),1));
+rho(1-5*h <= x & x <= 1+5*h,1) = round(rand(sum(1-5*h <=x & x<= 1+5*h),1));
+% cut off stuff below 1
+rho(x<=1,1) = 0;
 
 %% Boundary Conditions
 
@@ -44,13 +46,33 @@ i = 2:nx-1;
 
 %% Lax-Wendroff
 
+% for dt = 1:nt
+%     
+%     rho(i,dt+1) = rho(i,dt) - (a*k/(2*h))*(rho(i+1,dt)-rho(i-1,dt)) + (a*a*k*k/(2*h*h))*(rho(i-1,dt) - 2*rho(i,dt) + rho(i+1,dt));
+%     % Doubling Boundary Conditions
+%     % rho(x==1,dt+1) = rho(x==1,dt) - (a*k/(2*h))*(rho(x==1+h,dt)-rho(end,dt)) + (a*a*k*k/(2*h*h))*(rho(end,dt) - 2*rho(x==1,dt) + rho(x==1+h,dt));
+%     rho(x==1,dt+1) = 2*rho(end,dt);
+%     rho(end,dt+1) = rho(end,dt) - (a*k/(2*h))*(rho(x==1,dt)-rho(end-1,dt)) + (a*a*k*k/(2*h*h))*(rho(end-1,dt) - 2*rho(end,dt) + rho(x==1,dt));
+%     
+% end
+
+%% Beam-Warming
+
+% define i to account for one-sided method
+i = 3:nx-1;
+
 for dt = 1:nt
     
-    rho(i,dt+1) = rho(i,dt) - (a*k/(2*h))*(rho(i+1,dt)-rho(i-1,dt)) + (a*a*k*k/(2*h*h))*(rho(i-1,dt) - 2*rho(i,dt) + rho(i+1,dt));
-    % Periodic Boundary Conditions
-    rho(x==1,dt+1) = rho(x==1,dt) - (a*k/(2*h))*(rho(x==1+h,dt)-2*rho(end,dt)) + (a*a*k*k/(2*h*h))*(rho(end,dt) - 2*rho(x==1,dt) + rho(x==1+h,dt));
-    rho(end,dt+1) = rho(end,dt) - (a*k/(2*h))*(rho(x==1,dt)-rho(end-1,dt)) + (a*a*k*k/(2*h*h))*(rho(end-1,dt) - 2*rho(end,dt) + rho(x==1,dt));
-    
+    rho(i,dt+1) = rho(i,dt) - (a*k/(2*h))*(3*rho(i,dt)-4*rho(i-1,dt)+rho(i-2,dt)) + ...
+                  (a*a*k*k/(2*h*h))*(rho(i,dt)-2*rho(i-1,dt)+rho(i-2,dt));
+    rho(x==1+h,dt+1) = rho(x==1+h,dt) - (a*k/h)*(rho(x==1+h,dt)-rho(x==1,dt));
+% Boundary conditions
+%   rho(x==1,dt+1) = rho(x==1,dt) - (a*k/(2*h))*(3*rho(x==1,dt)-2*4*rho(end,dt)+rho(end-1,dt)) + ...
+%                    (a*a*k*k/(2*h*h))*(rho(x==1,dt)-2*rho(end,dt)+rho(end-1,dt));
+    rho(x==1,dt+1) = 2*rho(end,dt);
+    rho(end,dt+1) = rho(end,dt) - (a*k/(2*h))*(3*rho(end,dt)-4*rho(end-1,dt)+rho(end-2,dt)) + ...
+                  (a*a*k*k/(2*h*h))*(rho(end,dt)-2*rho(end-1,dt)+rho(end-2,dt));
+
 end
 
 %% Plotting
@@ -58,6 +80,8 @@ end
 rho_max = max(rho, [], 'all');
 rho_min = min(rho, [], 'all');
 figure(1);
+
+% put time in top of graph
 
 for dt = 1:nt
     plot(x(2:end),rho(:,dt),'b');
--- symmetric_pde_model/simple_advection_2D.m
+++ symmetric_pde_model/simple_advection_2D.m
@@ -6,8 +6,8 @@
 a = [1 2]; % velocity vector - may want to make into a function later
 h = 0.1; % spatial step size
 k = 0.05; % time step size
-x_bounds = [0 2];
-y_bounds = [0 2];
+x_bounds = [0 3];
+y_bounds = [0 3];
 t_bounds = [0 10];
 nx = (x_bounds(2) - x_bounds(1))/h; % number of x steps
 ny = (y_bounds(2) - y_bounds(1))/h; % number of y steps
@@ -15,6 +15,9 @@ nt = (t_bounds(2) - t_bounds(1))/k; % number of time steps
 
 x = x_bounds(1):h:x_bounds(2); % grid points
 y = y_bounds(1):h:y_bounds(2); % grid points
+% ensure the ends line up with rho
+x(end) = [];
+y(end) = [];
 
 rho = zeros(nx,ny,nt); % initialize density matrix
 
@@ -31,29 +34,42 @@ rho(x==1,y==1,1) = 1;
 
 %% Lax-Wendroff Method
 % free node indices
-% i = 2:nx-1;
-% j = 2:ny-1;
-% for dt=1:nt
-%     
-%     rho(i,j,dt+1) = rho(i,j,dt) - ...
-%                     (a(1)*k/(2*h))*(rho(i+1,j,dt)-rho(i-1,j,dt)) - ...
-%                     (a(2)*k/(2*h))*(rho(i,j+1,dt)-rho(i,j-1,dt)) + ...
-%                     (a(1)*a(1)*k*k/(h*h*2))*(rho(i+1,j,dt)-2*rho(i,j,dt)+rho(i-1,j,dt)) + ...
-%                     (a(2)*a(2)*k*k/(h*h*2))*(rho(i,j+1,dt)-2*rho(i,j,dt)+rho(i,j-1,dt));
-%     
-% end
-
-% Beam-Warming method
-% 
-i = 3:nx-1; % shifted forward to allow the boundary conditions to be set properly
-j = 3:ny-1;
+i = 2:nx-1;
+j = 2:ny-1;
 for dt=1:nt
+    
     rho(i,j,dt+1) = rho(i,j,dt) - ...
-                    (a(1)*k/(2*h))*(3*rho(i,j,dt)-4*rho(i-1,j,dt)+rho(i-2,j,dt)) - ...
-                    (a(1)*k/(2*h))*(3*rho(i,j,dt)-4*rho(i,j-1,dt)+rho(i,j-2,dt)) + ...
-                    (a(1)*a(1)*k*k/(h*h*2))*(rho(i,j,dt)-2*rho(i-1,j,dt)+rho(i-2,j,dt)) + ...
-                    (a(1)*a(1)*k*k/(h*h*2))*(rho(i,j,dt)-2*rho(i,j-1,dt)+rho(i,j-2,dt));
+                    (a(1)*k/(2*h))*(rho(i+1,j,dt)-rho(i-1,j,dt)) - ...
+                    (a(2)*k/(2*h))*(rho(i,j+1,dt)-rho(i,j-1,dt)) + ...
+                    (a(1)*a(1)*k*k/(h*h*2))*(rho(i+1,j,dt)-2*rho(i,j,dt)+rho(i-1,j,dt)) + ...
+                    (a(2)*a(2)*k*k/(h*h*2))*(rho(i,j+1,dt)-2*rho(i,j,dt)+rho(i,j-1,dt));
+% Boundary Conditions
+    rho(x==1,y>=1,dt+1) = 2*rho(x==2,y>=2,dt);
+    rho(x==2,y>=2,dt+1) = rho(x==2,y>=2,dt) - ...
+                          (a(1)*k/(2*h))*(rho(x==2+h,y>=2,dt)-rho(x==2-h,y>=2,dt)) - ...
+                          (a(2)*k/(2*h))*(rho(x==2,y>=2+h,dt)-rho(x==2,y>=2-h,dt)) + ...
+                          (a(1)*a(1)*k*k/(h*h*2))*(rho(x==2+h,y>=2,dt)-2*rho(x==2,y>=2,dt)+rho(x==2-h,y>=2,dt)) + ...
+                          (a(2)*a(2)*k*k/(h*h*2))*(rho(x==2,y>=2+h,dt)-2*rho(x==2,y>=2,dt)+rho(x==2,y>=2-h,dt));
+    rho(x>=1,y==1,dt+1) = 2*rho(x>=2,y==2,dt);
+    rho(x>=2,y==2,dt+1) = rho(x>=2,y==2,dt) - ...
+                          (a(1)*k/(2*h))*(rho(x>=2+h,y==2,dt)-rho(x>=2-h,y==2,dt)) - ...
+                          (a(2)*k/(2*h))*(rho(x>=2,y==2+h,dt)-rho(x>=2,y==2-h,dt)) + ...
+                          (a(1)*a(1)*k*k/(h*h*2))*(rho(x>=2+h,y==2,dt)-2*rho(x>=2,y==2,dt)+rho(x>=2-h,y==2,dt)) + ...
+                          (a(2)*a(2)*k*k/(h*h*2))*(rho(x>=2,y==2+h,dt)-2*rho(x>=2,y==2,dt)+rho(x>=2,y==2-h,dt));
+    
 end
+
+%% Beam-Warming method
+% 
+% i = 3:nx-1; % shifted forward to allow the boundary conditions to be set properly
+% j = 3:ny-1;
+% for dt=1:nt
+%     rho(i,j,dt+1) = rho(i,j,dt) - ...
+%                     (a(1)*k/(2*h))*(3*rho(i,j,dt)-4*rho(i-1,j,dt)+rho(i-2,j,dt)) - ...
+%                     (a(1)*k/(2*h))*(3*rho(i,j,dt)-4*rho(i,j-1,dt)+rho(i,j-2,dt)) + ...
+%                     (a(1)*a(1)*k*k/(h*h*2))*(rho(i,j,dt)-2*rho(i-1,j,dt)+rho(i-2,j,dt)) + ...
+%                     (a(1)*a(1)*k*k/(h*h*2))*(rho(i,j,dt)-2*rho(i,j-1,dt)+rho(i,j-2,dt));
+% end
 
 
 %% Plotting
